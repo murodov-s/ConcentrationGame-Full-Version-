a983089cabc8a16b442cbc1265dc2ae626be2586
Safarlibek Murodov <murodov_s@auca.kg>
2020-11-12 14:35:18 +0530

    Initial Commit

---
+++ ConcentrationGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConcentrationGame
+//
+//  Created by user181171 on 11/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
